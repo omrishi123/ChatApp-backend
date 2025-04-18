9211ff24ae034bcefe85231dd18506a31a48086a
omrishi123 <omrishi9608@gmail.com>
2025-04-18 23:46:18 +0530

    Update scripts with new MongoDB URI

--- scripts/listUsers.js
+++ scripts/listUsers.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const User = require('../models/User');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(async () => {
+const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://omrishi9608:omrishi9608@chatapp-cluster.thog6qs.mongodb.net/?retryWrites=true&w=majority&appName=chatapp-cluster';
+
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(async () => {
   const users = await User.find({});
   console.log(users);
   process.exit(0);
